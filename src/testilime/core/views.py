6c7ea78a643f52cfb55b0e2781bb1720332ed54b
Dzakwan Silverdi <dsilverdi@gmail.com>
2024-01-01 23:59:17 +0700

    insert and fetch projects

--- src/testilime/core/views.py
+++ src/testilime/core/views.py
@@ -6,14 +6,38 @@ from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET, require_http_methods, require_POST
 from testilime.core.forms import CreateProjectForm
+from testilime.core.models import Projects
 
 @never_cache
-@require_GET
+@require_http_methods(["GET", "POST"])
 @login_required
 def dashboard_view(request):
     context = {}
 
     form = CreateProjectForm(request.POST or None)
     context["form"] = form
+
+    if request.method == "POST":
+        if form.is_valid():
+            try:
+                name = form.cleaned_data["name"]
+                header_title = form.cleaned_data["header_title"]
+                message = form.cleaned_data["message"]
+                enable_ratings = form.cleaned_data["enable_ratings"]
+
+                # Create Projects instance and save it to the database
+                _ = Projects.objects.create(
+                    user=request.user,
+                    name=name,
+                    header_title=header_title,
+                    message=message,
+                    enable_ratings=enable_ratings,
+                )
+
+            except Exception as e:
+                form.add_error(None, "Error When Creating Project")
+
+    projects = Projects.objects.filter(user=request.user)
+    context["projects"] = projects
 
     return render(request, "core/pages/dashboard.html", context)
