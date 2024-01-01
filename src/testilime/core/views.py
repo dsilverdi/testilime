2204a7453d49570c77a07109e0dca5d59823178b
Dzakwan Silverdi <dslvr@dzakwan>
2024-01-01 23:01:54 +0700

    project forms view

--- src/testilime/core/views.py
+++ src/testilime/core/views.py
@@ -5,9 +5,14 @@ from django.shortcuts import redirect, render
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET, require_http_methods, require_POST
+from testilime.core.forms import CreateProjectForm
 
 @never_cache
 @require_GET
 def dashboard_view(request):
     context = {}
+
+    form = CreateProjectForm(request.POST or None)
+    context["form"] = form
+
     return render(request, "core/pages/dashboard.html", context)
